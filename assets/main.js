949f036a045012d3714063269b1617f7a5c66213
Austin Mayer <austin@austinmayerdesign.com>
2019-10-31 12:15:46 -0500

    pixelbook start

--- assets/main.js
+++ assets/main.js
@@ -1,22 +1,19 @@
 $(document).ready(function() {
-  if ($(window).width() <= 991) {
-    $('a[href*="#"]').on("click", function(e) {
-      e.preventDefault();
+  $('a[href*="#"]').on("click", function(e) {
+    e.preventDefault();
 
-      $("html, body").animate(
-        {
-          scrollTop: $($(this).attr("href")).offset().top
-        },
-        500,
-        "linear"
-      );
-    });
-    $(".fa-bars").on("click", function() {
-      $("#burger-menu").toggle(250);
-    });
-    $("#burger-menu a").on("click", function() {
-      $("#burger-menu").toggle(250);
-    });
-  } else {
-  }
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top
+      },
+      500,
+      "linear"
+    );
+  });
+  $(".fa-bars").on("click", function() {
+    $("#burger-menu").toggle(250);
+  });
+  $("#burger-menu a").on("click", function() {
+    $("#burger-menu").toggle(250);
+  });
 });
