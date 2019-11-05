9570cc6ec549e7579241a52de670e1bf9aacd1e9
Austin Mayer <austin@austinmayerdesign.com>
2019-11-04 19:41:28 -0600

    Finished Package Modal

--- assets/main.js
+++ assets/main.js
@@ -16,4 +16,36 @@ $(document).ready(function() {
   $("#burger-menu a").on("click", function() {
     $("#burger-menu").toggle(250);
   });
+
+  $("#gold-button").on("click", function() {
+    $("#overlay").fadeIn(1000);
+    $("#gold-modal").fadeIn(500);
+  });
+  $(".close").on("click", function() {
+    $("#overlay").fadeOut(1000);
+    $(".menu-modal").fadeOut(500);
+  });
+
+  $("#silver-button").on("click", function() {
+    $("#overlay").fadeIn(1000);
+    $("#silver-modal").fadeIn(500);
+  });
+  $(".close").on("click", function() {
+    $("#overlay").fadeOut(1000);
+    $(".menu-modal").fadeOut(500);
+  });
+
+  $("#bronze-button").on("click", function() {
+    $("#overlay").fadeIn(1000);
+    $("#bronze-modal").fadeIn(500);
+  });
+  $(".close").on("click", function() {
+    $("#overlay").fadeOut(1000);
+    $(".menu-modal").fadeOut(500);
+  });
+
+  $("#overlay").on("click", function() {
+    $("#overlay").fadeOut(1000);
+    $(".menu-modal").fadeOut(500);
+  });
 });
